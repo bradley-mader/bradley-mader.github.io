10c286b4dcf108aa2fa95699366e1809529d2c6c
mader <mader.bradley@gmail.com>
2023-03-04 02:35:30 -0800

    First pass at resume and update profile

--- portfolio/src/components/Home/ProfileBanner.js
+++ portfolio/src/components/Home/ProfileBanner.js
@@ -8,20 +8,12 @@ const Banner = styled.div`
     overflow: hidden;
 `;
 
-const ProfileImageContainer = styled.div`
-    position:relative;
-    padding: 2em;
-    margin-left:50%;
-    width:100%;
-    height:50vh;
-    transform:translate(-50%,0%);
-    overflow:hidden;
-`;
-
+const ProfileContainer = styled.div`
+    text-align: center;
+`
 const ProfileImage = styled.img`
-    height:100%;
-    border-radius:.25rem;
-
+    border-radius:50%;
+    margin-top:25vh;
 `
 
 export class ProfileBanner extends Component {
@@ -39,55 +31,13 @@ export class ProfileBanner extends Component {
 
     render() {
         return (
-            <Banner>    
-                <Card style={{
-                    background: "linear-gradient(to left top, #6B9B8A, #F3FFE3) fixed", margin: "5rem 10rem 5rem 10rem"
-                }}>
-                    <CardImg src='/profile_image.jpg' />
-                    <CardBody>
-                        <Row>
-                            <Col xs="6">
-                                <Card>
-                                    <CardBody>
-                                        <CardTitle tag="h5">Projects</CardTitle>
-                                        <CardSubtitle tag="h6" className="mb-2 text-muted">What have have worked on recently?</CardSubtitle>
-                                        <CardText></CardText>
-                                    </CardBody>
-                                </Card>
-                            </Col>
-                            <Col xs="6">
-                                <Card>
-                                    <CardBody>
-                                        <CardTitle tag="h5">Languages</CardTitle>
-                                        <CardSubtitle tag="h6" className="mb-2 text-muted">What programming languages do I have experience with?</CardSubtitle>
-                                        <CardText></CardText>
-                                    </CardBody>
-                                </Card>
-                            </Col>
-                        </Row>
-                        <Row style={{ marginTop: "2em" }}>
-                            <Col xs="6">
-                                <Card>
-                                    <CardBody>
-                                        <CardTitle tag="h5">Technologies</CardTitle>
-                                        <CardSubtitle tag="h6" className="mb-2 text-muted">What other development technologies have I worked with?</CardSubtitle>
-                                        <CardText></CardText>
-                                    </CardBody>
-                                </Card>
-                            </Col>
-                            <Col xs="6">
-                                <Card>
-                                    <CardBody>
-                                        <CardTitle tag="h5">Ideologies</CardTitle>
-                                        <CardSubtitle tag="h6" className="mb-2 text-muted">What systems and frameworks for development am I passionate about?</CardSubtitle>
-                                        <CardText></CardText>
-                                    </CardBody>
-                                </Card>
-                            </Col>
-                        </Row>
-
-                    </CardBody>
-                </Card>
+            <Banner>   
+                <ProfileContainer> 
+                    <ProfileImage src='profile_image_round.png'/>
+                    <h1>Bradley Mader</h1>
+                    <h3>Software Engineer</h3>
+                    <h3>Online/Backend</h3>
+                </ProfileContainer>
             </Banner>
         );
     }
--- portfolio/src/components/Resume/Resume.js
+++ portfolio/src/components/Resume/Resume.js
@@ -3,14 +3,20 @@ import { Row, Col, Card, CardBody, CardTitle, CardSubtitle, CardText, Button, Co
 
 import styled from 'styled-components';
 
+import "./Resume.css";
+
 const ResumeContainer = styled.div`
-        border: solid 2px #bbbbbb;
-        background-color: #eeeeee;
-        margin: 1em;
-        padding: 1em;
-        border-radius: 5px;
+      position: relative;
+      border: solid 2px #bbbbbb;
+      background-color: #eeeeee;
+      margin: 1em;
+      padding: 1em;
+      border-radius: 5px;
     `;
 
+const PrintHeader = styled.div`
+      height:2.5em;
+    `;
 const PrintIcon = styled.img`
         float:right;
         width:25px;
@@ -25,6 +31,15 @@ export class Resume extends Component {
     
   }
 
+  print(){     
+    var newWin = window.open('', '_blank', 'toolbar=0,location=0,menubar=0');
+    newWin.document.head.innerHTML = window.document.head.innerHTML;
+    newWin.document.head.innerHTML += '<title>Bradley Mader - Software Engineer</title>';
+    newWin.document.body.innerHTML = document.getElementById('print_container').innerHTML;
+    newWin.print();
+    newWin.close();
+  }
+
   render() {
     return (
         <div>
@@ -32,17 +47,108 @@ export class Resume extends Component {
                 Resume
             </h1>
             <ResumeContainer>
-                <PrintIcon className="not_printable" src="print.png"/>
-                <h1>Bradley Mader</h1>
-                <h5>Software Engineer</h5>
-                Experienced Full-Stack and Tools engineer with demonstrated expertise building backend web services and APIs, constructing and maintaining CI/CD pipelines, working with cloud technologies including Azure, Docker, Kubernetes and more in the FinTech and Gaming Industries.
-                <h2>Work Experience</h2>
-                <h3>Software Engineer</h3>
-                <h5>Electronic Arts</h5>
-                <h3>Software Developer</h3>
-                <h5>Boeing Employees' Credit Union</h5>
-                <h3>Performance Analyst</h3>
-                <h5>Boeing Employees' Credit Union</h5>
+                <PrintHeader>
+                  <PrintIcon src="print.png" onClick={this.print}/>
+                </PrintHeader>
+                <div id="print_container">
+                  <div id='Resume_Body'>
+                    <div id='Resume_Banner'>
+                      <div id='Resume_TitleContainer'>
+                        <p id="Resume_Name" className='full_break'>Bradley Mader</p>
+                        <p id="Resume_ProfileStatement">
+                          Experienced Backend Engineer with demonstrated expertise building backend web services and REST APIs, 
+                          constructing and maintaining CI/CD pipelines, working with cloud technologies including Azure, Docker, 
+                          Kubernetes and more in the FinTech and Gaming Industries.
+                        </p>
+                      </div>
+                      <div id="Resume_ContactInfo">
+                        <p><label className='Resume_ContactLabel'>Website:</label> bradley-mader.github.io</p>
+                        <p><label className='Resume_ContactLabel'>LinkedIn:</label> linkedin.com/in/bradley-mader</p>
+                        <p><label className='Resume_ContactLabel'>Itch Page:</label> bmader23.itch.io</p>
+                        <p><label className='Resume_ContactLabel'>Email:</label> mader.bradley@gmail.com</p>
+                        <p><label className='Resume_ContactLabel'>Twitter:</label> @BMMader</p>
+                      </div>
+                    </div>
+                    <h4 className='full_break'>Work Experience</h4>
+                    <div className='section_content'>
+                      <div className="work_title_bar">
+                        <div className='work_title'>Electronic Arts - Software Engineer</div>
+                        <div className='work_daterange'>July 2022 - Present</div>
+                      </div>
+                      <ul>
+                        <li>Worked with internal game teams to integrate new Frostbite Engine releases during ongoing game development.</li>
+                        <li>Engineered solutions to UX issues in the Frostbite Engine WPF Editor.</li>
+                        <li>Assisted game teams in implementing CI/CD pipelines to allow early detection of breaking changes in engine code.</li>
+                        <li>Collaborated with domain engineers to broaden interfaces and reduce need for artisonal solutions to overlapping problems between client teams.</li>
+                      </ul>
+                      <div className="work_title_bar">
+                        <div className='work_title'>Boeing Employees' Credit Union - Software Developer</div>
+                        <div className='work_daterange'>October 2018 - July 2022</div>
+                      </div>
+                      <ul>
+                        <li>Developed Customer Relationship Management application used by all customer facing employees of the 
+                        third largest Credit Union in the United States.</li>
+                        <li>Implemented Backend .NET Core Microservices with an n-tier architecture.</li>
+                        <li>Prototyped and implemented Azure Patterns which were later adopted across the organization including API 
+                          Manager, Functions and Container Instances hosting Docker images.</li>
+                        <li>Extracted data from SQL databases across multiple domains.</li>
+                        <li>Collaborated with internal and external engineers to determine architecture and implement connections to 
+                          a diverse array of web services including REST and SOAP APIs.</li>
+                      </ul>
+                      <div className="work_title_bar">
+                        <div className='work_title'>Boeing Employees' Credit Union - Performance Analyst</div>
+                        <div className='work_daterange'>October 2018 - July 2022</div>
+                      </div>
+                      <ul>
+                        <li>
+                          Generated staff and customer volume forecasting models which drove actual headcount within 1% of budget across 
+                          5 geographic regions.
+                        </li><li> 
+                        Engineered pilot scheduling platform to feed information from forecasting models back to 
+                        individual managers. 
+                        </li><li>
+                        Created Quality Assurance platform which drove the first decentralized quality initiative.
+                        </li>
+                      </ul>
+                    </div>
+                    <h4 className='full_break'>Education</h4>
+                    <div className='section_content'>
+                      <div className="work_title_bar">
+                        <div className='work_title'>B.S. in Computer Science - Oregon State University</div>
+                        <div className='work_daterange'>June 2015</div>
+                      </div>
+                      <div className="work_title_bar">
+                        <div className='work_title'>B.S. in Biology - Washington State University</div>
+                        <div className='work_daterange'>June 2012</div>
+                      </div>
+                    </div>
+                    <h4 className='full_break'>Skills</h4>
+                    <div className='section_content'>
+                      <div className='Resume_SkillCategory'>
+                        <p className='Resume_SkillCategoryTitle'></p>
+                        <div className='Resume_Skill Resume_PrimarySkill'>C#</div>
+                          <div className='Resume_Skill Resume_SecondarySkill Resume_LastSkillInSet'>WebApi2</div>
+                        <div className='Resume_Skill Resume_PrimarySkill'>Go</div>
+                          <div className='Resume_Skill Resume_SecondarySkill'>Gin</div>
+                          <div className='Resume_Skill Resume_SecondarySkill'>Gorilla Websockets</div>
+                          <div className='Resume_Skill Resume_SecondarySkill'>Google OAuth2</div>
+                          <div className='Resume_Skill Resume_SecondarySkill'>Golang JWT</div>
+                          <div className='Resume_Skill Resume_SecondarySkill'>Gin</div>
+                          <div className='Resume_Skill Resume_SecondarySkill Resume_LastSkillInSet'>Validator</div>
+                        <div className='Resume_Skill Resume_PrimarySkill'>C++</div>
+                          <div className='Resume_Skill Resume_SecondarySkill Resume_LastSkillInSet'>SDL2</div>
+                        <div className='Resume_Skill Resume_PrimarySkill Resume_LastSkillInSet'>t-SQL</div>
+                        <div className='Resume_Skill Resume_PrimarySkill'>Javascript</div>
+                          <div className='Resume_Skill Resume_SecondarySkill Resume_LastSkillInSet'>React</div>
+                        <div className='Resume_Skill Resume_PrimarySkill Resume_LastSkillInSet'>Azure</div>
+                        <div className='Resume_Skill Resume_PrimarySkill Resume_LastSkillInSet'>Docker</div>
+                        <div className='Resume_Skill Resume_PrimarySkill Resume_LastSkillInSet'>Kubernetes</div>
+                      </div>
+                      
+
+                    </div>
+                  </div>
+                </div>
             </ResumeContainer>
         </div>
     );
