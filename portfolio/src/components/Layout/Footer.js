41784eb7abff32bd6e52bf9d203d5b92916cf9ec
mader <mader.bradley@gmail.com>
2023-03-10 21:02:00 -0800

    add workflow action

--- portfolio/src/components/Layout/Footer.js
+++ portfolio/src/components/Layout/Footer.js
@@ -14,9 +14,9 @@ const ContactChannelIcon = styled.img`
 export const Footer = () =>
 {
   return (<FooterBox>
-        <a href="https://linkedin.com/in/bradley-mader" target="_blank"><ContactChannelIcon src="twitter.svg"></ContactChannelIcon></a>
+        <a href="https://twitter.com/BMMader" target="_blank"><ContactChannelIcon src="twitter.svg"></ContactChannelIcon></a>
         <a href="https://bmader23.itch.io" target="_blank"><ContactChannelIcon src="itchio.svg"></ContactChannelIcon></a>
         <a href="mailto:mader.bradley@gmail.com"><ContactChannelIcon src="email_logo.png"></ContactChannelIcon></a>
-        <a href="https://twitter.com/BMMader" target="_blank"><ContactChannelIcon src="linkedin.png"></ContactChannelIcon></a>
+        <a href="https://linkedin.com/in/bradley-mader" target="_blank"><ContactChannelIcon src="linkedin.png"></ContactChannelIcon></a>
     </FooterBox>);
 }
