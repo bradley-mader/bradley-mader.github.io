6bee70c1e728d60f3b90ac5e9288f77611384ecd
mader <mader.bradley@gmail.com>
2023-03-11 01:06:05 -0800

    Add appropriate packages

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+import 'bootstrap/dist/css/bootstrap.css';
 
 import './style/style.scss';
 
