5f936b61f3313d2db2f7fe1ecbc96781bbdb04e3
mader <mader.bradley@gmail.com>
2023-03-10 22:01:44 -0800

    Add Medium link and icon.

--- portfolio/src/components/Layout/Footer.js
+++ portfolio/src/components/Layout/Footer.js
@@ -16,6 +16,7 @@ export const Footer = () =>
   return (<FooterBox>
         <a href="https://twitter.com/BMMader" target="_blank"><ContactChannelIcon src="twitter.svg"></ContactChannelIcon></a>
         <a href="https://bmader23.itch.io" target="_blank"><ContactChannelIcon src="itchio.svg"></ContactChannelIcon></a>
+        <a href="https://medium.com/@mader.bradley" target="_blank"><ContactChannelIcon src="medium.png"></ContactChannelIcon></a>
         <a href="mailto:mader.bradley@gmail.com"><ContactChannelIcon src="email_logo.png"></ContactChannelIcon></a>
         <a href="https://linkedin.com/in/bradley-mader" target="_blank"><ContactChannelIcon src="linkedin.png"></ContactChannelIcon></a>
     </FooterBox>);
