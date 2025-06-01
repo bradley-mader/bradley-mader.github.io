c912850a183415dcc94ea6857d714467c842dd80
Brad Mader <bmader@ea.com>
2025-06-01 01:49:13 -0700

    Update intro

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -99,9 +99,8 @@ export class Home extends Component {
                     <InterludeText className='text-light left-justified m-font'>My name is</InterludeText>
                     <FirstNameText className='text-bright xxl-font left-justified script-font'>Bradley</FirstNameText>
                     <LastNameText className='text-bright xxl-font left-justified script-font'>Mader<span className='text-primary left-justified sans-serif-font xxl-font'>.</span></LastNameText>
-                    <DescriptionText className='text-light m-font left-justified'>I am a Backend Software Engineer with nearly 7 years of experience focused primarily on distributed applications.
-                    Go | Kubernetes | PostgresQL | AWS | Azure
-                    </DescriptionText>
+                    <DescriptionText className='text-light m-font left-justified'>I am a Backend Software Engineer with nearly 7 years of experience focused primarily on distributed applications.</DescriptionText>
+                    <DescriptionText className='text-light m-font left-justified'>Go | Kubernetes | PostgresQL | AWS | Azure</DescriptionText>
                 </ProfileContainer>
             </Banner>
         );
